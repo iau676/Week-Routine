ba891f86e8feab265872427c91d555a6a3f5b35e
iau676 <103315521+iau676@users.noreply.github.com>
2024-03-20 23:58:23 +0300

    cleanup

--- Week Routine/Model/RoutineBrain.swift
+++ Week Routine/Model/RoutineBrain.swift
@@ -38,6 +38,24 @@ struct RoutineBrain {
         saveContext()
     }
     
+    mutating func updateRoutine(routine: Routine, title: String, day: Int, hour: Int, minute: Int, color: String, soundInt: Int) {
+        routine.title = title
+        routine.day = Int16(day)
+        routine.hour = Int16(hour)
+        routine.minute = Int16(minute)
+        routine.color = color
+        routine.ascending = Int16(hour * 66 + minute)
+        routine.soundInt = Int64(soundInt)
+        NotificationManager.shared.updateRoutineNotification(routine: routine)
+        saveContext()
+    }
+    
+    mutating func deleteRoutine(_ routine: Routine) {
+        deleteNotification(routine: routine)
+        context.delete(routine)
+        saveContext()
+    }
+    
     mutating func addLog(routine: Routine) {
         let newLog = Log(context: self.context)
         newLog.date = Date()
@@ -46,6 +64,122 @@ struct RoutineBrain {
         
         routine.addToLogs(newLog)
         saveContext()
+    }
+    
+    mutating func deleteLog(_ routine: Routine, _ index: Int) {
+        context.delete(routine.logArray[index])
+        saveContext()
+    }
+    
+    mutating func loadRoutineArray(with request: NSFetchRequest<Routine> = Routine.fetchRequest()){
+        do {
+            request.sortDescriptors = [NSSortDescriptor(key: "ascending", ascending: true)]
+            routineArray = try context.fetch(request)
+        } catch {
+           print("Error fetching data from context \(error)")
+        }
+    }
+    
+    func saveContext() {
+        do {
+          try context.save()
+        } catch {
+           print("Error saving context \(error)")
+        }
+    }
+    
+    //MARK: - Notification
+    
+    func deleteNotification(routine: Routine) {
+        guard let uuid = routine.uuid else { return }
+        let dayInt = routine.day
+        switch dayInt {
+            case 8:
+            NotificationManager.shared.removeNotification(id: "\(uuid)wr2")
+            NotificationManager.shared.removeNotification(id: "\(uuid)wr3")
+            NotificationManager.shared.removeNotification(id: "\(uuid)wr4")
+            NotificationManager.shared.removeNotification(id: "\(uuid)wr5")
+            NotificationManager.shared.removeNotification(id: "\(uuid)wr6")
+            case 9:
+            NotificationManager.shared.removeNotification(id: "\(uuid)wr7")
+            NotificationManager.shared.removeNotification(id: "\(uuid)wr1")
+            default:
+            NotificationManager.shared.removeNotification(id: uuid)
+        }
+        saveContext()
+    }
+    
+    func updateNotification(routine: Routine) {
+        let currentNotify = routine.isNotify
+        
+        if !currentNotify {
+            if let title = routine.title,
+                let color = routine.color,
+                let uuid = routine.uuid {
+                NotificationManager.shared.addNotification(title: title, dayInt: Int(routine.day),
+                                                           hour: Int(routine.hour), minute: Int(routine.minute),
+                                                           color: color, soundInt: Int(routine.soundInt),
+                                                           id: uuid)
+            }
+        } else {
+            deleteNotification(routine: routine)
+        }
+        
+        routine.isNotify = !currentNotify
+        saveContext()
+    }
+    
+    //MARK: - Freeze
+    
+    func updateFrozen(routine: Routine) {
+        let currentFrozen = routine.isFrozen
+        
+        if currentFrozen {
+            if let title = routine.title,
+                let color = routine.color,
+                let uuid = routine.uuid {
+                NotificationManager.shared.addNotification(title: title, dayInt: Int(routine.day),
+                                                           hour: Int(routine.hour), minute: Int(routine.minute),
+                                                           color: color, soundInt: Int(routine.soundInt),
+                                                           id: uuid)
+            }
+        } else {
+            deleteNotification(routine: routine)
+        }
+        
+        routine.isFrozen = !currentFrozen
+        routine.isNotify = currentFrozen
+        saveContext()
+    }
+    
+    //MARK: - Helpers
+    
+    func findRoutines(for selectedSegmentIndex: Int) -> [Int] {
+        var tempArray = [Int]()
+        let array = brain.routineArray
+        
+        for i in 0..<array.count {
+            let routine = array[i]
+            switch selectedSegmentIndex {
+            case 0: if routine.day == 0 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
+            case 1: if routine.day == 1 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
+            case 2: if routine.day == 2 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
+            case 3: if routine.day == 3 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
+            case 4: if routine.day == 4 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
+            case 5: if routine.day == 5 || routine.day == 7 || routine.day == 9 { tempArray.append(i) }
+            case 6: if routine.day == 6 || routine.day == 7 || routine.day == 9 { tempArray.append(i) }
+            default: break
+            }
+        }
+        return tempArray
+    }
+    
+    mutating func findRoutine(uuid: String, completion: (Routine)-> Void) {
+        loadRoutineArray()
+        if let routine = routineArray.first(where: {$0.uuid == uuid}) {
+            deleteNotification(routine: routine)
+            completion(routine)
+        }
     }
     
     func checkCompletedToday(routine: Routine, selectedSegmentIndex: Int) -> Bool {
@@ -95,144 +229,8 @@ struct RoutineBrain {
         return "\(hourStr):\(minStr)"
     }
     
-    mutating func updateRoutine(routine: Routine, title: String, day: Int, hour: Int, minute: Int, color: String, soundInt: Int) {
-        routine.title = title
-        routine.day = Int16(day)
-        routine.hour = Int16(hour)
-        routine.minute = Int16(minute)
-        routine.color = color
-        routine.ascending = Int16(hour * 66 + minute)
-        routine.soundInt = Int64(soundInt)
-        NotificationManager.shared.updateRoutineNotification(routine: routine)
-        saveContext()
-    }
-    
-    mutating func deleteRoutine(_ routine: Routine) {
-        deleteNotification(routine: routine)
-        context.delete(routine)
-        saveContext()
-    }
-    
-    func deleteNotification(routine: Routine) {
-        guard let uuid = routine.uuid else { return }
-        let dayInt = routine.day
-        switch dayInt {
-            case 8:
-            NotificationManager.shared.removeNotification(id: "\(uuid)wr2")
-            NotificationManager.shared.removeNotification(id: "\(uuid)wr3")
-            NotificationManager.shared.removeNotification(id: "\(uuid)wr4")
-            NotificationManager.shared.removeNotification(id: "\(uuid)wr5")
-            NotificationManager.shared.removeNotification(id: "\(uuid)wr6")
-            case 9:
-            NotificationManager.shared.removeNotification(id: "\(uuid)wr7")
-            NotificationManager.shared.removeNotification(id: "\(uuid)wr1")
-            default:
-            NotificationManager.shared.removeNotification(id: uuid)
-        }
-        saveContext()
-    }
-    
-    mutating func deleteLog(_ routine: Routine, _ index: Int) {
-        context.delete(routine.logArray[index])
-        saveContext()
-    }
-    
-    func updateFrozen(routine: Routine) {
-        let currentFrozen = routine.isFrozen
-        
-        if currentFrozen {
-            if let title = routine.title,
-                let color = routine.color,
-                let uuid = routine.uuid {
-                NotificationManager.shared.addNotification(title: title, dayInt: Int(routine.day),
-                                                           hour: Int(routine.hour), minute: Int(routine.minute),
-                                                           color: color, soundInt: Int(routine.soundInt),
-                                                           id: uuid)
-            }
-        } else {
-            deleteNotification(routine: routine)
-        }
-        
-        routine.isFrozen = !currentFrozen
-        routine.isNotify = currentFrozen
-        saveContext()
-    }
-    
-    func updateNotification(routine: Routine) {
-        let currentNotify = routine.isNotify
-        
-        if !currentNotify {
-            if let title = routine.title,
-                let color = routine.color,
-                let uuid = routine.uuid {
-                NotificationManager.shared.addNotification(title: title, dayInt: Int(routine.day),
-                                                           hour: Int(routine.hour), minute: Int(routine.minute),
-                                                           color: color, soundInt: Int(routine.soundInt),
-                                                           id: uuid)
-            }
-        } else {
-            deleteNotification(routine: routine)
-        }
-        
-        routine.isNotify = !currentNotify
-        saveContext()
-    }
-    
-    mutating func loadRoutineArray(with request: NSFetchRequest<Routine> = Routine.fetchRequest()){
-        do {
-            request.sortDescriptors = [NSSortDescriptor(key: "ascending", ascending: true)]
-            routineArray = try context.fetch(request)
-        } catch {
-           print("Error fetching data from context \(error)")
-        }
-    }
-    
-    func saveContext() {
-        do {
-          try context.save()
-        } catch {
-           print("Error saving context \(error)")
-        }
-    }
-    
-    //MARK: - Helpers
-    
-    func findRoutines(for selectedSegmentIndex: Int) -> [Int] {
-        var tempArray = [Int]()
-        let array = brain.routineArray
-        
-        for i in 0..<array.count {
-            let routine = array[i]
-            switch selectedSegmentIndex {
-            case 0: if routine.day == 0 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
-            case 1: if routine.day == 1 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
-            case 2: if routine.day == 2 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
-            case 3: if routine.day == 3 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
-            case 4: if routine.day == 4 || routine.day == 7 || routine.day == 8 { tempArray.append(i) }
-            case 5: if routine.day == 5 || routine.day == 7 || routine.day == 9 { tempArray.append(i) }
-            case 6: if routine.day == 6 || routine.day == 7 || routine.day == 9 { tempArray.append(i) }
-            default: break
-            }
-        }
-        return tempArray
-    }
-    
-    mutating func findRoutine(uuid: String, completion: (Routine)-> Void) {
-        loadRoutineArray()
-        if let routine = routineArray.first(where: {$0.uuid == uuid}) {
-            deleteNotification(routine: routine)
-            completion(routine)
-        }
-    }
-    
     func getTodayDate() -> String{
         return Date().getFormattedDate(format: "yyyy-MM-dd")
-    }
-    
-    func getDayInt() -> Int {
-        var day = Calendar.current.component(.weekday, from: Date())
-        day = (day-2 < 0) ? 6 : day-2
-        return day
     }
     
     func getHour() -> Int {
@@ -243,6 +241,20 @@ struct RoutineBrain {
     func getMinute() -> Int {
         let minute = Calendar.current.component(.minute, from: Date())
         return minute
+    }
+    
+    func getDayInt() -> Int {
+        //returns an integer from 1 - 7, with 1 being Sunday and 7 being Saturday -
+        //1 - Sunday
+        //2 - Monday
+        //3 - Tuesday
+        //4 - Wednesday
+        //5 - Thursday
+        //6 - Friday
+        //7 - Saturday
+        var day = Calendar.current.component(.weekday, from: Date())
+        day = (day-2 < 0) ? 6 : day-2
+        return day
     }
     
     func getDayName(_ dayInt: Int16) -> String {
